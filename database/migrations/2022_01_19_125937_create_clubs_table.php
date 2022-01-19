40add32d194abf9fbfcf71394a3ef24b9b3d2836
Sergio Morote <morote99@gmail.com>
2022-01-19 14:23:10 +0100

    migracion,modelo,controlador clubes

---
+++ database/migrations/2022_01_19_125937_create_clubs_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClubsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clubs', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('club_img');
+            $table->string('club_banner');
+            $table->string('direction');
+            $table->string('tlf');
+            $table->string('email')->unique();
+            $table->json('services');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clubs');
+    }
+}
