17993b0bf6709c843e618a9e7cc92920f18c1a7f
DonBermudas <bermudezh.sergio@gmail.com>
2022-01-23 21:43:54 +0100

    Comienzo con registro y listado de clubs

--- app/Http/Controllers/ClubsController.php
+++ app/Http/Controllers/ClubsController.php
@@ -2,9 +2,78 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Club;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Auth;
 
 class ClubsController extends Controller
 {
-    //
+    /**
+     * Función para registrar clubs
+     */
+    public function register(Request $request){
+
+        $validatedData = Validator::make($request->all(),[
+            'name' => 'bail|required|string|max:255', // Bail: stop running validation rules on an attribute after the first validation failure
+            'club_img' => 'string|max:255|nullable',
+            'club_banner' => 'string|max:255|nullable',
+            'direction' => 'required|string|max:255',
+            'tlf' => 'required|string|regex:/[0-9]{9}/',
+            'email' => 'bail|required|string|email|max:255|unique:clubs'
+        ],
+        [
+            'name.required' => 'Introduzca un nombre para el club',
+            'name.string' => 'El nombre debe ser un String',
+            'name.max' => 'El nombre no puede superar 255 caracteres',
+            'direction.required' => 'Intruduzca una dirección para el club',
+            'direction.string' => 'La dirección debe ser un String',
+            'direction.max' => 'La dirección no puede superar 255 caracteres',
+            'tlf.required' => 'Introduzca un teléfono para el club',
+            'tlf.regex' => 'El teléfono debe contener 9 números',
+            'email.required' => 'Introduce un email correcto',
+            'email.string' => 'El email debe ser un string',
+            'email.email' => 'Introduce formato válido de email',
+            'email.unique' => 'Este email ya está registrado'
+        ]);
+
+        if ($validatedData->fails()) {
+            return $this->sendError('Datos incorrectos, club no registrado', $validatedData->errors()->all(),406);
+        }else{
+            try{
+                $club = Club::create([
+                'name' => $request->input('name'),
+                'club_img' => $request->input('club_img'),
+                'club_banner' => $request->input('club_banner'),
+                'direction' => $request->input('direction'),
+                'tlf' => $request->input('tlf'),
+                'email' => $request->input('email')
+                ]);
+
+                $token = $club->createToken('auth_token')->plainTextToken; // En otro vídeo aquí ponen el mismo nombre que la variable $token (en su caso accessToken)
+
+                return response()->json([
+                    //'club' => $club, // Creo que no es necesario
+                    'access_Token' => $token,
+                    'token_type' => 'Bearer'
+                ]);
+
+            }catch(\Exception $e){
+                //return $this->sendError('Usuario no registrado', $e->getMessage(),406); 
+                return response()->json([
+                    'message' => 'Error al registrar el club'
+                ], 401); 
+            }
+        }
+
+    }
+
+    /**
+     * Función para listar todos los clubs
+     */
+    public function listClubs(Request $request){
+        $clubs = Club::all();
+        return $clubs;
+    }
+
 }
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ClubsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,3 +23,11 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::get('/userinfo', [AuthController::class, 'infouser'])->middleware('auth:sanctum');
+
+// CLUBS
+Route::post('/registerclub', [ClubsController::class, 'register']);
+
+Route::middleware(['auth:sanctum'])->group(function () {
+     // Preguntar si esto es otra manera de señalar que va a pasar por el middleware
+});
+Route::get('/listclubs', [ClubsController::class, 'listClubs'])/*->middleware('auth:sanctum')*/;
