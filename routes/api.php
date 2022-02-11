d7f0f3e52a4ab3756b192adb2e80269a5ad62e68
Diego <diego171200@gmail.com>
2022-02-11 09:17:45 +0100

    Check Merge Eventos into diego

--- routes/api.php
+++ routes/api.php
@@ -24,23 +24,17 @@ Route::get('/userinfo', [AuthController::class, 'infouser'])->middleware('auth:s
 Route::post('/recoverpass', [AuthController::class, 'recoverPass']);
 Route::post('/usermodify', [AuthController::class, 'modifyUser'])->middleware('auth:sanctum');
 Route::post('/passmodify', [AuthController::class, 'modifyPass'])->middleware('auth:sanctum');
-<<<<<<< HEAD
+// Diego Upload Image and User Exits
 Route::post('/getUploadImage', [AuthController::class, 'getUploadImage']);
 Route::post('/checkIfUserExists', [AuthController::class, 'checkIfUserExists']);
-=======
 Route::get('/listevents', [AuthController::class, 'listevents']); // Tendrá que pasar por un middleware
 Route::get('/listfavs', [AuthController::class, 'listfavs'])->middleware('auth:sanctum');
 Route::get('/searchclubs', [AuthController::class, 'searchClubs'])->middleware('auth:sanctum');
 Route::post('/joinevent', [AuthController::class, 'joinEvent'])->middleware('auth:sanctum');
->>>>>>> Eventos
 
 // Rutas Clubes
 Route::post('/registerclub', [ClubsController::class, 'register']);
 Route::get('/listclubs', [ClubsController::class, 'listClubs'])->middleware('auth:sanctum');
 Route::post('/registerfavclub', [ClubsController::class, 'registerFavClub'])->middleware('auth:sanctum'); // El user tendrá que estar logeado para poder añadir clubes favoritos
-<<<<<<< HEAD
 Route::post('/registercourt', [CourtsController::class, 'courtRegister']);
-=======
 Route::post('/registerevent', [ClubsController::class, 'registerEvent']); // Tendrá que pasar por un middleware para poder registrarlos
-
->>>>>>> Eventos
