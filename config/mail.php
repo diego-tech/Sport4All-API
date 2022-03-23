6fb26a0127ce9b97141e3ffd01fe0a7f8f1db2ad
Diego Muñoz Herranz <diego171200@gmail.com>
2022-03-23 18:55:51 +0100

    Add Config to mail.php

--- config/mail.php
+++ config/mail.php
@@ -38,6 +38,10 @@ return [
             'transport' => 'smtp',
             'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
             'port' => env('MAIL_PORT', 587),
+            'from' => [
+                'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
+                'name' => env('MAIL_FROM_NAME', 'Example'),
+            ],
             'encryption' => env('MAIL_ENCRYPTION', 'tls'),
             'username' => env('MAIL_USERNAME'),
             'password' => env('MAIL_PASSWORD'),
